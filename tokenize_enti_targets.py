83827429695bff8870f1993a15819cca5286e7b9
YankeeMarco <15118160186@qq.com>
2018-08-01 23:00:57 +0800

    fix loop bug

--- tokenize_enti_targets.py
+++ tokenize_enti_targets.py
@@ -60,38 +60,39 @@ class tokenization():
             使用正则提取对应的简称或全称，如果有顿号，把那些字串也分开提取，作为标注的标的，当然是先
             把字符长度小的匹配出来，分词之后也是先把长度长的连起来。没问题的"""
         res_paired = {}  # 临时定义一个res的列表，存储修改后的train res
-        for index, result_row in enumerate(res_rows):
+        for x in range(len(res_rows)):
+            res_row = res_rows[x]
+            for  y in  range(6):
+                res_paired[str(x)+str(y)]= [re.split(r'\t', res_row)[y]]
 
-            for indi, res_value in enumerate(re.split('\t', result_row)):
-                if indi == 0:
-                    continue
-                if indi in [1, 4, 5]:
-                    res_paired[str(index)+str(indi)] = [res_value]
-                    continue
-                res_value_list = res_value.split('、')
-                res_paired[str(index)+str(indi)] = res_value_list
+        for arrow_str in entities_arrows_list:
 
-                for res_value_split in res_value_list:
-                    if res_value_split in entities:
-                        for arrow_str in entities_arrows_list:
-                            if res_value_split in arrow_str:
+            for index, result_row in enumerate(res_rows):
+
+                for indi, res_value in enumerate(re.split('\t', result_row)):
+                    if indi in [0, 1, 4, 5]:
+                        continue
+                    res_value_list = res_value.split('、')
+
+                    for res_value_split in res_value_list:
+                        if res_value_split in entities and res_value_split in arrow_str:
                         # 找出配对的简称或者全称，添加,如果是股权/估值法/金额直接添加并且continue
-                                niki, fullna = re.split(r'~', arrow_str)
-                                fullna_first = fullna.split('，')[0]
-                                niki_split_list = re.split(r'[/、]', niki)
-                        # 对应的全称满足三个条件，长度/逗号  以及含有简称的几个字
-                                if res_value_split in niki_split_list \
-                                        and len(fullna_first) < 18 \
-                                        and re.search(re.sub('(?<=\w)', '?', res_value_split), fullna_first):
-                                    res_paired[str(index)+str(indi)].append(fullna_first)
-                                """ 由全称查简称时候要避免 公司/本公司/上市公司/发起人/申请人/,
-                                    含有这几个字的要剔除  """
-                                if res_value_split == fullna_first:
-                                    # 对应的简称满足几个条件： 包含在全程里面，不长于4个字，不等于
-                                    for niki_split in niki_split_list:
-                                        if re.search(re.sub('(?<=\w)', '?', fullna_first), niki_split)\
-                                                and not re.search(r'(^公司$|^本公司&|^上市公司&|人$|资产|标的|交易|对方|发行|对象|股东|对手|单位)',niki_split):
-                                            res_paired[str(index)+str(indi)].append(niki_split)
+                            niki, fullna = re.split(r'~', arrow_str)
+                            fullna_first = fullna.split('，')[0]
+                            niki_split_list = re.split(r'[/、]', niki)
+                    # 对应的全称满足三个条件，长度/逗号  以及含有简称的几个字
+                            if res_value_split in niki_split_list \
+                                    and len(fullna_first) < 18 \
+                                    and re.search(re.sub('(?<=\w)', '?', res_value_split), fullna_first):
+                                res_paired[str(index)+str(indi)].append(fullna_first)
+                            """ 由全称查简称时候要避免 公司/本公司/上市公司/发起人/申请人/,
+                                含有这几个字的要剔除  """
+                            if res_value_split == fullna_first:
+                                # 对应的简称满足几个条件： 包含在全程里面，不长于4个字，不等于
+                                for niki_split in niki_split_list:
+                                    if re.search(re.sub('(?<=\w)', '?', fullna_first), niki_split)\
+                                            and not re.search(r'(^公司$|^本公司$|^上市公司$|人$|资产|标的|交易|对方|发行|对象|股东|对手|单位)',re.sub(r'\s', '', niki_split)):
+                                        res_paired[str(index)+str(indi)].append(niki_split)
 
 
         # 遍历公告的每一句，把每一句送进模型。
@@ -101,21 +102,16 @@ class tokenization():
             words = ' '.join(words)
             words = words+' '+'。'+' '  # 加上句号以及句号后面的空格
             for ent in entities:
-                countit +=1
-                print(countit)
-            for ent in entities:
                 # 把words中所有是实体的中间去掉空格。使用双层sub
                 # 正则还是要多注释啊
                 """ re.sub(r'(?<=\w)(?=\w)'','\s?',ent) 是把实体里面的每个字符中间插入“\s?”
                 表示匹配任何以此序列出现但中间可能有空格的情况,分词之后join成空格分割的。然后找出words
                 中出现这个序列的地方，将其换成没空格的"""
                 if len(ent) > 1:
-                    if '\*' in ent:
-                        ent = re.sub(r'\*', '', ent)
-                    if not re.search(r'([\d.]+%的?(?:股权|股份|权益))', ent):
+                    if not re.search(r'([\d.]+%的?(?:股权|股份|权益))', ent):  # 如果没有股权关键字，直接加上空格匹配pattern
                         patt_ent = re.sub(r'(?<=\w)(?=\w)', r'\s?', ent)
-                    elif len(ent) > 7:
-                        patt_ent = re.sub(r'(?<=\w)(?=\w)',r'\s?', re.split(r'(?=的?[\d.]+%的?(?:股权|股份|权益))', ent)[0])
+                    elif len(ent) > 7: # 如果有股权关键字，如果长度比较长，就把前面主体提出来，单独分词
+                        patt_ent = re.sub(r'(?<=\w)(?=\w)',r'\s?', re.sub(r'[^\s]+的?[\d.]+%的?(股权|股份|权益)','', ent))
                     else:
                         patt_ent = re.sub(r'(?<=\w)(?=\w)', r'\s?', ent)
                     # 下面一句把words中所有符合主体列表的项目，可能被分词分开的，重新合并起来，单独成行
@@ -128,11 +124,11 @@ class tokenization():
                 目的是好的，就是让模型更容易找到目标，模型不需要判断开始和结束，
                 但是这样的正则太难了， 我无法将所有合适的实体
                 全部抽出来，而导致标注的缺失，那么还是把任务给模型了"""
-            for index, tags_list in res_paired:
+            for index, tags_list in res_paired.items():
                 # 表中的小表，可能有一个或多个成员，遍历一下,包括顿号分割的那些都可以标出来了，不影响合并好的实体字符串。
-                    for sub_res in tags_list:
-                        if len(sub_res) >1:
-                            words= re.sub(r'(?<={})(?=\n)'.format(sub_res), '\t{}'.format(index),words)
+                for sub_res in tags_list:
+                    if len(sub_res) > 1:
+                        words= re.sub(r'{}(?=\n)'.format(sub_res), '\n{}\t{}\n'.format(sub_res, index), words)
                 # train——result标注完了，现在标注o,就是把非数字结尾的行加上tab和o
             words = re.sub(r'(?<!\t\d)(?=\n)', '\to', words)
             with open('output_test_tokenization.txt', 'a') as af:
