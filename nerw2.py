225630577aa1f2639aaa8460fdde82249b34d872
YankeeMarco <15118160186@qq.com>
2018-08-03 23:10:21 +0800

    lost in python-crfsuite

--- nerw2.py
+++ nerw2.py
@@ -15,44 +15,50 @@ def tokenit(path1):
     with open(path1,'r') as rf:
         strr = rf.read()
         list_x_y = re.split(r'[\n\t]', strr)
-        x_train = list_x_y[0::2]
+        x_train = list_x_y[0::2][:-1]
         y_train = list_x_y[1::2]
-    return x_train[:-1], y_train
+        return x_train, y_train
 x_train = []
 y_train =[]
 
-for  i in  os.listdir('/home/47_7/FDDC_datasets_dir/tokenized_datasets_for_anago/chongzu/')[:1000]:
+for  i in  os.listdir('/home/47_7/FDDC_datasets_dir/tokenized_datasets_for_anago/chongzu/')[0:3]:
     x,y =  tokenit('/home/47_7/FDDC_datasets_dir/tokenized_datasets_for_anago/chongzu/'+i)
-    x_train += x
-    y_train += y
+    if len(x)==len(y):
+        x_train += x
+        y_train += y
+    if len(x_train)==len(y_train):
+        print("It is OK {}".format(i))
+    else:
+        print("baaaaad")
 
 trainer = pycrfsuite.Trainer(verbose=True)
 # x = data_generator_x()
 # # y = data_generator_y()
-for i in range(700):
-    trainer.append(x_train, y_train)
+# for i in range(700):
+trainer.append(x_train, y_train)
 
- # Submit training data to the trainer
+# Submit training data to the trainer
 # for xseq, yseq in zip(x_train, y_train):
 #     print("hh")
 #     trainer.append(xseq, yseq)
 
 # Set the parameters of the model
-    trainer.set_params({
-        # coefficient for L1 penalty
-        'c1': 0.1,
+trainer.set_params({
+    # coefficient for L1 penalty
+    'c1': 0.1,
 
-        # coefficient for L2 penalty
-        'c2': 0.01,
+    # coefficient for L2 penalty
+    'c2': 0.01,
 
-        # maximum number of iterations
-        'max_iterations': 200,
+    # maximum number of iterations
+    'max_iterations': 200,
 
-        # whether to include transitions that
-        # are possible, but not observed
-        'feature.possible_transitions': True
-    })
+    # whether to include transitions that
+    # are possible, but not observed
+    'feature.possible_transitions': True
+})
 
-    # Provide a file name as a parameter to the train function, such that
-    # the model will be saved to the file when training is finished
-    trainer.train('/home/47_7/FDDC_datasets_dir/tokenized_datasets_for_anago/crf.model')
+# Provide a file name as a parameter to the train function, such that
+# the model will be saved to the file when training is finished
+print("start at {} longtime training.........".format(time.ctime()))
+trainer.train('/home/47_7/FDDC_datasets_dir/tokenized_datasets_for_anago/crf.model')
